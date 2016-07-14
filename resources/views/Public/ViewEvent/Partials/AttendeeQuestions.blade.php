72e2b906030f6efadedae24ec38a28578e3fcab6
Nelson Antunes <nantunes@pentaho.com>
2016-07-14 23:55:50 +0100

    VD 6

--- resources/views/Public/ViewEvent/Partials/AttendeeQuestions.blade.php
+++ resources/views/Public/ViewEvent/Partials/AttendeeQuestions.blade.php
@@ -1,4 +1,5 @@
 @foreach($ticket->questions->sortBy('sort_order') as $question)
+@if($question->is_enabled == 1)
     <div class="col-md-12">
         <div class="form-group">
             {!! Form::label("ticket_holder_questions[{$ticket->id}][{$i}][$question->id]", $question->title, ['class' => $question->is_required ? 'required' : '']) !!}
@@ -37,4 +38,5 @@
 
         </div>
     </div>
+@endif
 @endforeach
