3d4107059d57aa62f7d4c0a3ded5dfca508003d8
Nelson Antunes <nantunes@pentaho.com>
2016-09-07 23:50:23 +0100

    VD1

--- app/Http/Controllers/EventCheckoutController.php
+++ app/Http/Controllers/EventCheckoutController.php
@@ -488,9 +488,9 @@ class EventCheckoutController extends Controller
             if (isset($ticket_order['transaction_id'])) {
                 $order->transaction_id = $ticket_order['transaction_id'][0];
             }
-            if ($ticket_order['order_requires_payment'] && !isset($request_data['pay_offline']) ) {
+            /*if ($ticket_order['order_requires_payment'] && !isset($request_data['pay_offline']) ) {
                 $order->payment_gateway_id = $ticket_order['payment_gateway']->id;
-            }
+            }*/
             $order->first_name = $request_data['order_first_name'];
             $order->last_name = $request_data['order_last_name'];
             $order->email = $request_data['order_email'];
